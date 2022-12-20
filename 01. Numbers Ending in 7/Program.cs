3b52809fdb5dd2ca12a9ced11df82ac3c82204f8
Ivon Dimitrova <ivon.p.dimitrova@gmail.com>
2022-12-20 23:39:13 +0200

    Add project files.

---
+++ 01. Numbers Ending in 7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01._Numbers_Ending_in_7_ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //отпечатва числата в диапазона от 1 до 1000, които завършват на 7.
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 10 == 7)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+    }
+}
