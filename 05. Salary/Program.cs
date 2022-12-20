9d606b20629db223c371c6501fa642e7a42296c4
Ivon Dimitrova <117980916+ivoniD@users.noreply.github.com>
2022-12-20 23:49:07 +0200

    Add files via upload

---
+++ 05. Salary/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _05._Salary_ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double salary = double.Parse(Console.ReadLine());
+            double penalty = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                string site = Console.ReadLine();
+
+                if (site == "Facebook") // 150
+                {
+                    penalty = penalty + 150;
+                }
+                else if (site == "Instagram") // 100
+                {
+                    penalty = penalty + 100;
+                }
+                else if (site == "Reddit") // 50
+                {
+                    penalty = penalty + 50;
+                }
+                if (penalty >= salary)
+                {
+                    break;
+                }
+            }
+            if (penalty >= salary)
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+            else
+            {
+                Console.WriteLine(salary - penalty);
+            }
+        }
+    }
+}
